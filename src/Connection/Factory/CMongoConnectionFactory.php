8cca1ed5904e29d16b9499f3591d7320d05bad03
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-09-27 13:13:36 +0300

    Initial commit

---
+++ src/Connection/Factory/CMongoConnectionFactory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-mongo
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-mongo
+ */
+
+namespace Vain\Mongo\Connection\Factory;
+
+/**
+ * Class CMongoConnectionFactory
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class CMongoConnectionFactory extends AbstractConnectionFactory
+{
+}
